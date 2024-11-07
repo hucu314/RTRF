e3e2d4e5034920ec54c2de26cf039d24e99aa8a4
hucu314 <hcumpean@ftstudent.org>
2024-11-07 14:04:19 -0500

    idk

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -25,35 +25,13 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 func dirAnimation(direction:Vector2):
-	var move = 'none'
-	if direction.x == 0 and direction.y == 0 :
-		move = 'idle'
-		if direction.x > 0 and direction.y == 0:
-			player.set_animation(move+'Right')
-		if direction.x < 0 and direction.y == 0:
-			player.set_animation(move+'Left')
-		if direction.x == 0 and direction.y > 0:
-			player.set_animation(move+'Front')
-		if direction.x == 0 and direction.y < 0:
-			player.set_animation(move+'Back')
-	else:
-		move = 'move' 
-		if direction.x > 0 and direction.y == 0:
-			player.set_animation(move+'Right')
-		if direction.x < 0 and direction.y == 0:
-			player.set_animation(move+'Left')
-		if direction.x == 0 and direction.y > 0:
-			player.set_animation(move+'Front')
-		if direction.x == 0 and direction.y < 0:
-			player.set_animation(move+'Back')
-func dirAnimation(direction:Vector2):
-	print(direction)
 	if direction.x > 0 and direction.y == 0:
-		return 'Right'
+		player.set_animation(move+'Right')
 	if direction.x < 0 and direction.y == 0:
-		return 'Left'
+		player.set_animation(move+'Left')
 	if direction.x == 0 and direction.y > 0:
-		return 'Front'
+		player.set_animation(move+'Front')
 	if direction.x == 0 and direction.y < 0:
-		return 'Right'
+		player.set_animation(move+'Back')
+
 	move_and_slide()
