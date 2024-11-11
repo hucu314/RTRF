3bf419afd017799cca4329558f66948a46211668
hucu314 <hcumpean@ftstudent.org>
2024-11-11 14:07:00 -0500

    Player Animations/Movement
    Finished basic player animations and basic movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 @onready var player: AnimatedSprite2D = $player
 @onready var clothes: AnimatedSprite2D = $clothes
 @onready var last_position: Vector2
-
+@onready var dir
 const SPEED = 600.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	dirAnimation(global_position-last_position)
+	#dirAnimation(global_position-last_position)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -16,22 +16,30 @@ func _physics_process(delta: float) -> void:
 	var ydirection := Input.get_axis("up","down")
 	print(xdirection)
 	print(ydirection)
+	playDir(xdirection,ydirection)
+	print(dir)
 	if xdirection or ydirection:
 		velocity.x = xdirection * SPEED
 		velocity.y = ydirection * SPEED
-			
+		changeAnim('move',dir)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
-func dirAnimation(direction:Vector2):
-	if direction.x > 0 and direction.y == 0:
-		player.set_animation(move+'Right')
-	if direction.x < 0 and direction.y == 0:
-		player.set_animation(move+'Left')
-	if direction.x == 0 and direction.y > 0:
-		player.set_animation(move+'Front')
-	if direction.x == 0 and direction.y < 0:
-		player.set_animation(move+'Back')
-
+		changeAnim('idle',dir)
 	move_and_slide()
+
+func changeAnim(movement,direction):
+	player.play(str(movement)+str(direction))
+	clothes.play(str(movement)+str(direction))
+
+func playDir(x,y):
+	print(x)
+	print(y)
+	if x<0 and y == 0:
+		dir = 'Left'
+	if x>0 and y == 0:
+		dir = 'Right'
+	if x==0 and y < 0:
+		dir = 'Up'
+	if x==0 and y > 0:
+		dir = 'Down'
